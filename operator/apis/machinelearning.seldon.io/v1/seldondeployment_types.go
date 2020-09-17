2f8c2a6d17ac89e212af5701c7237d576194092b
Jun Chen <chenjun@caicloud.io>
2020-09-17 14:40:40 +0800

    feat: Support diversion by http header (#3)

--- operator/apis/machinelearning.seldon.io/v1/seldondeployment_types.go
+++ operator/apis/machinelearning.seldon.io/v1/seldondeployment_types.go
@@ -22,6 +22,7 @@ import (
 	"strconv"
 
 	"github.com/seldonio/seldon-core/operator/constants"
+	istio_networking "istio.io/api/networking/v1alpha3"
 	autoscalingv2beta2 "k8s.io/api/autoscaling/v2beta1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -223,6 +224,7 @@ type PredictorSpec struct {
 	Labels          map[string]string       `json:"labels,omitempty" protobuf:"bytes,7,opt,name=labels"`
 	SvcOrchSpec     SvcOrchSpec             `json:"svcOrchSpec,omitempty" protobuf:"bytes,8,opt,name=svcOrchSpec"`
 	Traffic         int32                   `json:"traffic,omitempty" protobuf:"bytes,9,opt,name=traffic"`
+	TrafficMatchs   []HTTPMatchRequest      `json:"trafficMatchs,omitempty" protobuf:"bytes,9,opt,name=trafficMatchs"`
 	Explainer       *Explainer              `json:"explainer,omitempty" protobuf:"bytes,10,opt,name=explainer"`
 	Shadow          bool                    `json:"shadow,omitempty" protobuf:"bytes,11,opt,name=shadow"`
 }
@@ -266,6 +268,59 @@ const (
 	AlibiIntegratedGradientsExplainer AlibiExplainerType = "IntegratedGradients"
 	AlibiALEExplainer                 AlibiExplainerType = "ALE"
 )
+
+// HTTPMatchRequest specify rules to match requests. All rules are ANDed.
+type HTTPMatchRequest struct {
+	// Match headers of a request.
+	Headers map[string]StringMatch `json:"headers"`
+}
+
+// StringMatch defines 3 different types of matching strategy, i.e. only match prefix,
+// exact string match, and regular expression match.
+type StringMatch struct {
+	Prefix  string `json:"prefix,omitempty"`
+	Exact   string `json:"exact,omitempty"`
+	Regex   string `json:"regex,omitempty"`
+	Include string `json:"include,omitempty"`
+	Exclude string `json:"exclude,omitempty"`
+}
+
+// ConvertIstioStringMatch convert the StringMatch type in this file to *istio_networking.StringMatch.
+func ConvertIstioStringMatch(match StringMatch) *istio_networking.StringMatch {
+	if match.Exact != "" {
+		return &istio_networking.StringMatch{
+			MatchType: &istio_networking.StringMatch_Exact{
+				Exact: match.Exact,
+			},
+		}
+	} else if match.Prefix != "" {
+		return &istio_networking.StringMatch{
+			MatchType: &istio_networking.StringMatch_Prefix{
+				Prefix: match.Prefix,
+			},
+		}
+	} else if match.Regex != "" {
+		return &istio_networking.StringMatch{
+			MatchType: &istio_networking.StringMatch_Regex{
+				Regex: match.Regex,
+			},
+		}
+	} else if match.Include != "" {
+		return &istio_networking.StringMatch{
+			MatchType: &istio_networking.StringMatch_Regex{
+				Regex: match.Include,
+			},
+		}
+	} else if match.Exclude != "" {
+		return &istio_networking.StringMatch{
+			MatchType: &istio_networking.StringMatch_Regex{
+				Regex: match.Exclude,
+			},
+		}
+	}
+
+	return nil
+}
 
 type Explainer struct {
 	Type               AlibiExplainerType `json:"type,omitempty" protobuf:"string,1,opt,name=type"`
--- operator/apis/machinelearning.seldon.io/v1/seldondeployment_webhook.go
+++ operator/apis/machinelearning.seldon.io/v1/seldondeployment_webhook.go
@@ -371,14 +371,27 @@ func (r *SeldonDeploymentSpec) checkPredictiveUnits(pu *PredictiveUnit, p *Predi
 func checkTraffic(spec *SeldonDeploymentSpec, fldPath *field.Path, allErrs field.ErrorList) field.ErrorList {
 	var trafficSum int32 = 0
 	var shadows int = 0
+	var trafficMatchsNum int = 0
 	for i := 0; i < len(spec.Predictors); i++ {
 		p := spec.Predictors[i]
+
+		if len(p.TrafficMatchs) != 0 {
+			trafficMatchsNum++
+			continue
+		}
+
 		trafficSum = trafficSum + p.Traffic
 
 		if p.Shadow == true {
 			shadows += 1
 		}
 	}
+
+	// Traffic diversion by header, not check traffic diversion by weight.
+	if trafficMatchsNum != 0 {
+		return allErrs
+	}
+
 	if trafficSum != 100 && (len(spec.Predictors)-shadows) > 1 {
 		allErrs = append(allErrs, field.Invalid(fldPath, spec.Predictors[0].Name, "Traffic must sum to 100 for multiple predictors"))
 	}
